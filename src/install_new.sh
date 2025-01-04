8c577b2a802aa9858385cfebd7c4949021646f60
DrDeadKnee <duncan.halverson@gmail.com>
2025-01-03 21:35:49 -0500

    Adding Conda

--- src/install_new.sh
+++ src/install_new.sh
@@ -34,3 +34,9 @@ apt-get -y install tree
 
 echo "Removing Superfluous Packages"
 apt autoremove
+
+echo "Installing Miniconda"
+mkdir -p ~/miniconda3
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
+bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
+rm ~/miniconda3/miniconda.sh
